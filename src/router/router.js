b4919848d35a10fc02a3deb7897c349c7e3ffbdf
JuozasPetryla <itsgrtjuozas@gmail.com>
2023-08-10 22:33:18 +0300

    history mode

--- src/router/router.js
+++ src/router/router.js
@@ -1,24 +1,24 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import PostsPage from '../views/PostsPage.vue'
-import PostDetailPage from '../views/PostDetailPage.vue'
-import AuthorsPage from '../views/AuthorsPage.vue'
-import AuthorDetailPage from '../views/AuthorDetailPage.vue'
-import ErrorPage from '../views/ErrorPage.vue'
+import PostsPage from "../views/PostsPage.vue";
+import PostDetailPage from "../views/PostDetailPage.vue";
+import AuthorsPage from "../views/AuthorsPage.vue";
+import AuthorDetailPage from "../views/AuthorDetailPage.vue";
+import ErrorPage from "../views/ErrorPage.vue";
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', component: PostsPage },
-    { path: '/postDetail/:id', component: PostDetailPage },
-    { path: '/authors', component: AuthorsPage },
-    { path: '/authorDetail/:id', component: AuthorDetailPage },
-    { path: '/*', name: 'error', component: ErrorPage },
+  { path: "/", component: PostsPage },
+  { path: "/postDetail/:id", component: PostDetailPage },
+  { path: "/authors", component: AuthorsPage },
+  { path: "/authorDetail/:id", component: AuthorDetailPage },
+  { path: "/*", name: "error", component: ErrorPage },
 ];
 
 const router = new VueRouter({
-    routes,
-
+  mode: "history",
+  routes,
 });
 
 export default router;
